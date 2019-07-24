300e57a3d3cb8d57f27f1a92ea17596733f67040
efrainmejiasc <e.mejias@innova4j.com>
2019-07-24 13:34:58 -0400

    Ruta

--- TercerVector/TercerVector/EngineDb.cs
+++ TercerVector/TercerVector/EngineDb.cs
@@ -14,33 +14,74 @@ namespace TercerVector
         //private readonly string cadenaConexion = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\TercerVector.mdb;Persist Security Info=True;Jet OLEDB:Database Password=1234";
         private static string cadenaConexion = ConfigurationManager.ConnectionStrings["TercerVector.Properties.Settings.TercerVectorConnectionString"].ToString();
 
-
-        public int InsertarActualizarClientePago(string SQL, int Id, string Nombre, string Apellido, string Cedula, string FechaPago, string FechaExpiracion, string NumeroTransferencia)
+        public int InsertarRuta(Ruta m)
         {
             int resultado = new int();
             OleDbConnection Conexion = new OleDbConnection(cadenaConexion);
             using (Conexion)
             {
-                OleDbCommand command = new OleDbCommand(SQL, Conexion);
+                OleDbCommand command = new OleDbCommand(EngineData.SQLRuta, Conexion);
                 command.CommandType = CommandType.Text;
                 Conexion.Open();
                 command.Parameters.Clear();
-                if (Apellido != string.Empty)
-                {
-                    command.Parameters.AddWithValue("@Nombre", Nombre + " " + Apellido);
-                }
-                else
-                {
-                    command.Parameters.AddWithValue("@Nombre", Nombre);
-                }
-                command.Parameters.AddWithValue("@Cedula", Cedula);
-                command.Parameters.AddWithValue("@FechaPago", Convert.ToDateTime(FechaPago));
-                command.Parameters.AddWithValue("@FechaExpiracion", Convert.ToDateTime(FechaExpiracion));
-                command.Parameters.AddWithValue("@NumeroTransferencia", NumeroTransferencia);
-                if (Id > 0)
-                {
-                    command.Parameters.AddWithValue("@Id", Id);
-                }
+                command.Parameters.AddWithValue("@Campo1", m.Campo1);
+                command.Parameters.AddWithValue("@Campo2", m.Campo2);
+                command.Parameters.AddWithValue("@Campo3", m.Campo3);
+                command.Parameters.AddWithValue("@Campo4", m.Campo4);
+                command.Parameters.AddWithValue("@Campo5", m.Campo5);
+                command.Parameters.AddWithValue("@Campo6", m.Campo6);
+                resultado = command.ExecuteNonQuery();
+                Conexion.Close();
+            }
+            return resultado;
+        }
+
+        public int InsertarMapa(Mapa m)
+        {
+            int resultado = new int();
+            OleDbConnection Conexion = new OleDbConnection(cadenaConexion);
+            using (Conexion)
+            {
+                OleDbCommand command = new OleDbCommand(EngineData.SQLMapa, Conexion);
+                command.CommandType = CommandType.Text;
+                Conexion.Open();
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@Uno", m.Uno);
+                command.Parameters.AddWithValue("@Dos", m.Dos);
+                command.Parameters.AddWithValue("@Tres", m.Tres);
+                command.Parameters.AddWithValue("@Cuatro",m.Cuatro);
+                command.Parameters.AddWithValue("@Cinco", m.Cinco);
+                command.Parameters.AddWithValue("@Seis", m.Seis);
+                command.Parameters.AddWithValue("@Siete", m.Siete);
+                command.Parameters.AddWithValue("@Ocho", m.Ocho);
+                command.Parameters.AddWithValue("@Nueve", m.Nueve);
+                command.Parameters.AddWithValue("@Diez", m.Diez);
+                command.Parameters.AddWithValue("@Once", m.Once);
+                command.Parameters.AddWithValue("@Doce", m.Doce);
+                command.Parameters.AddWithValue("@Trece", m.Trece);
+                command.Parameters.AddWithValue("@Catorce", m.Catorce);
+                command.Parameters.AddWithValue("@Quince", m.Quince);
+                command.Parameters.AddWithValue("@Diesiseis", m.Diesiseis);
+                command.Parameters.AddWithValue("@Diecisiete", m.Diesisiete);
+                command.Parameters.AddWithValue("@Diesiocho", m.Diesiocho);
+                command.Parameters.AddWithValue("@Diesinueve", m.Diesinueve);
+                command.Parameters.AddWithValue("@Veinte", m.Veinte);
+                command.Parameters.AddWithValue("@Veintiuno", m.Veintiuno);
+                command.Parameters.AddWithValue("@Veintidos", m.Veintidos);
+                command.Parameters.AddWithValue("@Veintitres", m.Veintitres);
+                command.Parameters.AddWithValue("@Veinticuatro", m.Veinticuatro);
+                command.Parameters.AddWithValue("@Veinticinco", m.Veinticinco);
+                command.Parameters.AddWithValue("@Veintiseis", m.Veintiseis);
+                command.Parameters.AddWithValue("@Veintisiete", m.Veintisiete);
+                command.Parameters.AddWithValue("@Veintiocho", m.Veintiocho);
+                command.Parameters.AddWithValue("@Veintinueve", m.Veintinueve);
+                command.Parameters.AddWithValue("@Treinta", m.Treinta);
+                command.Parameters.AddWithValue("@Treintayuno", m.Treintayuno);
+                command.Parameters.AddWithValue("@Treintaydos", m.Treintaydos);
+                command.Parameters.AddWithValue("@Treintaytres", m.Treintaytres);
+                command.Parameters.AddWithValue("@Treintaycuatro", m.Treintaycuatro);
+                command.Parameters.AddWithValue("@Treintaycinco", m.Treintaycinco);
+                command.Parameters.AddWithValue("@Treintayseis", m.Treintayseis);
                 resultado = command.ExecuteNonQuery();
                 Conexion.Close();
             }
---
+++ TercerVector/TercerVector/Mapa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TercerVector
+{
+    public class Mapa
+    {
+        public int Id { get; set; }
+        public string Uno { get; set; }
+        public string Dos { get; set; }
+        public string Tres { get; set; }
+        public string Cuatro { get; set; }
+        public string Cinco { get; set; }
+        public string Seis { get; set; }
+        public string Siete { get; set; }
+        public string Ocho{ get; set; }
+        public string Nueve { get; set; }
+        public string Diez { get; set; }
+        public string Once { get; set; }
+        public string Doce { get; set; }
+        public string Trece { get; set; }
+        public string Catorce { get; set; }
+        public string Quince { get; set; }
+        public string Diesiseis { get; set; }
+        public string Diesisiete { get; set; }
+        public string Diesiocho { get; set; }
+        public string Diesinueve { get; set; }
+        public string Veinte { get; set; }
+        public string Veintiuno { get; set; }
+        public string Veintidos { get; set; }
+        public string Veintitres { get; set; }
+        public string Veinticuatro { get; set; }
+        public string Veinticinco { get; set; }
+        public string Veintiseis { get; set; }
+        public string Veintisiete { get; set; }
+        public string Veintiocho { get; set; }
+        public string Veintinueve { get; set; }
+        public string Treinta { get; set; }
+        public string Treintayuno { get; set; }
+        public string Treintaydos { get; set; }
+        public string Treintaytres { get; set; }
+        public string Treintaycuatro { get; set; }
+        public string Treintaycinco { get; set; }
+        public string Treintayseis { get; set; }
+        public string Resultado { get; set; }
+
+    }
+}
